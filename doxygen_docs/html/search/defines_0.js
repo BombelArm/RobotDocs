1fd59229b527f08cf540f2dca2f96a508460a3d0
michal <michal.rom089@gmail.com>
2018-05-29 22:10:09 +0200

    soft link

--- doxygen_docs/html/search/defines_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['encoder0_5foffset',['ENCODER0_OFFSET',['../encoder__lib_8h.html#a7648391c1c975deb80467e5919192799',1,'encoder_lib.h']]],
-  ['encoder1_5foffset',['ENCODER1_OFFSET',['../encoder__lib_8h.html#acae55010435c7663fe0e7855be632a09',1,'encoder_lib.h']]],
-  ['encoder2_5foffset',['ENCODER2_OFFSET',['../encoder__lib_8h.html#a570253a97f13ae89e8d4d567a87583cd',1,'encoder_lib.h']]],
-  ['epsilon',['EPSILON',['../motion__control_8h.html#a002b2f4894492820fe708b1b7e7c5e70',1,'motion_control.h']]]
-];
--- doxygen_docs/html/search/defines_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['task_5fbuffer_5fsize',['TASK_BUFFER_SIZE',['../task__manager_8h.html#adf0da5898aa5f84e2d16826f367db78d',1,'task_manager.h']]]
-];
